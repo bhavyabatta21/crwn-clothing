df943debb9c88abeeb3d33c31d3bcc765a358d1e
Bhavya Batta (bbatta) <bbatta@cisco.com>
2020-08-26 23:48:37 +0530

    update the menu-item to navigate with HOC-higher order components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import {Route, Switch, Link} from 'react-router-dom'
 import './App.css';
 import HomePage from'./components/homepage.component'
+
+const HatsPage =()=>(
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage/>
+    <Switch>
+      <Route exact path='/' component={HomePage}></Route>
+      <Route path='/hats' component={HatsPage}></Route>
+    </Switch>
     </div>
   );
 }
